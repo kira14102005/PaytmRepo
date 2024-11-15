bc67e9a3688e7463e7542085f614a88e87d07bf4
Harshit Rai <raiharshit66@gmail.com>
2024-11-15 14:49:03 +0530

    ADDED BALANCE TABLE

--- backend/db/schema.js
+++ backend/db/schema.js
@@ -1,11 +1,43 @@
 const mong = require('mongoose')
 mong.connect('mongodb+srv://raiharshit66:WIHIYsLnhGeGzm1p@clusterr.qbnob.mongodb.net/paytmDB');
-const userSchema  =mong.Schema({
-    firstName : String,
-    lastName : String,
-    email : String,
-    password : String,
+const userSchema = mong.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    email: {
+        lowerCase: true,
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
 })
-const User = mong.model('UserTable' , userSchema);
+const balanceSchema = mong.Schema({
+    userId: {
+        type: mong.Schema.Types.ObjectId,      //REFERENCE to USER
+        ref: 'UserTable',
+        required: true,
+    },
+    balance: {
+        type: Number,
+        required: true,
+    }
+})
+const Balance = new mong.model('BalanceTable', balanceSchema)
+const User = mong.model('UserTable', userSchema);
 
-module.exports =  {User};
+module.exports = { User };
